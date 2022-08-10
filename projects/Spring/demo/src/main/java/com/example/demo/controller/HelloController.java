7b9da54997d42e57d2ef5b387c771999ee699ee4
taewoong <taewoong@gmail.com>
2022-08-10 23:00:08 +0900

    이태웅  - 스프링 demo 웹 어플리케이션

---
+++ projects/Spring/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.service.commonService;
+
+@Controller
+public class HelloController {
+
+    @Autowired
+    commonService service;
+
+    @RequestMapping("/")
+    public String SayHello(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
+
+        List<String> list = service.testDBConnection();
+        System.out.println(list);
+        list.add(name);
+        model.addAttribute("nameList", list);
+        model.addAttribute("currentTime", service.selectDbTime());
+        return "home";
+    }
+}
